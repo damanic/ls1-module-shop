dac3257fc9bb7bcecdebb57304ac7294b23904da
Matt <matt@mjman.net>
2018-08-01 01:51:56 +1000

    adds customer preferences helper

---
+++ helpers/shop_customerpreferences.php
@@ -0,0 +1,161 @@
+<?php
+
+class Shop_CustomerPreferences {
+
+	private static $cache = array();
+	private $table_name = 'shop_customer_preferences';
+	protected $preference_data = array();
+
+
+	public static function load( $customer, $field_name ) {
+		$field_name  = trim( $field_name );
+		$customer_id = is_numeric( $customer ) ? $customer : $customer->id;
+		if ( !$customer_id ) {
+			return false;
+		}
+		$_this  = new self();
+		$result = $_this->load_preference( $customer_id, $field_name );
+		return $result ? $_this : false;
+	}
+
+
+	public static function get( $customer, $field_name, $default = null ) {
+		$field_name  = trim( $field_name );
+		$customer_id = is_numeric( $customer ) ? $customer : $customer->id;
+		if ( !$customer_id ) {
+			return false;
+		}
+		if ( isset( self::$cache[$customer_id][$field_name] ) ) {
+			return self::$cache[$customer_id][$field_name];
+		}
+		$preference = self::load( $customer, $field_name );
+		return $preference ? $preference->value( $default ) : $default;
+	}
+
+	public static function get_by_hash( $hash, $default = null ) {
+		if ( isset( self::$cache[$hash] ) ) {
+			return self::$cache[$hash];
+		}
+		$_this = new self();
+		$found = $_this->load_preference_by_hash( $hash );
+		return $found ? $_this->value( $default ) : $default;
+	}
+
+	public static function set( $customer, $field_name, $value ) {
+		$field_name  = trim( $field_name );
+		$customer_id = is_numeric( $customer ) ? $customer : $customer->id;
+		if ( !$customer_id ) {
+			throw new Phpr_ApplicationException( 'Cannot set customer preference: Invalid customer ID provided' );
+		}
+		$_this = new self();
+		$_this->save_preference( $customer_id, $field_name, $value );
+
+		return $_this;
+	}
+
+	public static function set_by_hash( $hash, $value ) {
+		$_this = new self();
+		$found = $_this->load_preference_by_hash( $hash );
+		if ( $found ) {
+			return self::set( $_this->customer_id(), $_this->field(), $value );
+		}
+		return false;
+	}
+
+
+	public function value( $default = null ) {
+		if ( !key_exists( 'pref_value', $this->preference_data ) ) {
+			return $default;
+		}
+		return $this->preference_data['pref_value'];
+	}
+
+	public function hash() {
+		if ( !key_exists( 'pref_hash', $this->preference_data ) ) {
+			return null;
+		}
+		return $this->preference_data['pref_hash'];
+	}
+
+	public function customer_id() {
+		if ( !key_exists( 'customer_id', $this->preference_data ) ) {
+			return null;
+		}
+		return $this->preference_data['customer_id'];
+	}
+
+	public function field() {
+		if ( !key_exists( 'pref_field', $this->preference_data ) ) {
+			return null;
+		}
+		return $this->preference_data['pref_field'];
+	}
+
+
+	protected function save_preference( $customer_id, $field_name, $value ) {
+		$bind = array(
+			'customer_id' => $customer_id,
+			'pref_hash'   => $this->generate_preference_hash( $customer_id, $field_name ),
+			'pref_field'  => $field_name,
+			'pref_value'  => $value,
+		);
+
+		$sql = "INSERT INTO " . $this->table_name . "
+				(customer_id, pref_hash, pref_field, pref_value)
+				VALUES (:customer_id,:pref_hash,:pref_field, :pref_value)
+				ON DUPLICATE KEY UPDATE pref_value = :pref_value";
+
+		Db_DbHelper::query( $sql, $bind );
+		$this->load_preference( $customer_id, $field_name, $value );
+		self::$cache[$bind['pref_hash']]        = $this->value();
+		self::$cache[$customer_id][$field_name] = $this->value();
+
+		return true;
+	}
+
+	protected function load_preference( $customer_id, $field_name ) {
+		$bind = array(
+			'customer_id' => $customer_id,
+			'pref_field'  => $field_name,
+		);
+		$sql  = "SELECT * FROM " . $this->table_name . "
+				WHERE customer_id = :customer_id  
+				AND pref_field = :pref_field";
+
+		$result = Db_DbHelper::queryArray( $sql, $bind );
+		if ( !$result ) {
+			$this->preference_data = array();
+
+			return false;
+		}
+		$this->preference_data = $result[0];
+
+		return true;
+	}
+
+	protected function load_preference_by_hash( $hash ) {
+		$bind = array(
+			'pref_hash' => $hash,
+		);
+		$sql  = "SELECT * FROM " . $this->table_name . "
+				WHERE pref_hash = :pref_hash";
+
+		$result = Db_DbHelper::queryArray( $sql, $bind );
+		if ( !$result ) {
+			$this->preference_data = array();
+
+			return false;
+		}
+		$this->preference_data = $result[0];
+
+		return true;
+	}
+
+	protected function generate_preference_hash( $customer_id, $field_name ) {
+		$string = get_class( $this ) . $customer_id . $field_name . rand( 0, 1000 );
+
+		return md5( $string );
+	}
+
+
+}
