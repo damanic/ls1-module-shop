58d7d02d6167c78bd4c4a299a7040f9cc2525f7e
Matt <matt@mjman.net>
2019-01-25 10:08:16 +1100

    IPV6 Support

---
+++ updates/1.33.7.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `shop_orders` CHANGE `customer_ip` `customer_ip` VARCHAR(45);
+ALTER TABLE `shop_product_ratings` CHANGE `customer_ip` `customer_ip` VARCHAR(45);
+ALTER TABLE `shop_product_reviews` CHANGE `customer_ip` `customer_ip` VARCHAR(45);
+
+
