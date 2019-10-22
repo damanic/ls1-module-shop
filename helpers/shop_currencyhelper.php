f7ad368d7b7656b5e0f36b7ce7666cd927918b02
Matt <matt@mjman.net>
2019-10-22 14:35:19 +1100

    Cache currency helper lookups

--- helpers/shop_currencyhelper.php
+++ helpers/shop_currencyhelper.php
@@ -2,11 +2,21 @@
 
 class Shop_CurrencyHelper{
 
+	protected static $cache_currency_settings = array();
+
 	public static function format_currency($num,$decimals,$currency_code){
 		if (strlen($num) && strlen($currency_code)) {
 
-			$currencies = new Shop_CurrencySettings();
-			$obj = $currencies->where( 'code = :code || iso_4217_code = :code', array( 'code' => $currency_code ) )->find_all();
+			if(!isset(self::$cache_currency_settings[$currency_code])){
+				self::$cache_currency_settings[$currency_code] = null;
+				$currencies = new Shop_CurrencySettings();
+				$obj = $currencies->where( 'code = :code || iso_4217_code = :code', array( 'code' => $currency_code ) )->find_all();
+				if ( $obj ) {
+					self::$cache_currency_settings[$currency_code] = $obj;
+				}
+			}
+
+			$obj = self::$cache_currency_settings[$currency_code];
 
 			if ( $obj ) {
 				$negative   = $num < 0;
