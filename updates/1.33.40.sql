1d896306f6aff8585ae43c3117ebab0796e95ad2
Matt <matt@mjman.net>
2020-08-11 14:47:29 +1000

    Fix issues with payment dispute logging

---
+++ updates/1.33.40.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `shop_payment_transaction_disputes`
+DROP COLUMN `payment_method_id`,
+ADD COLUMN `shop_payment_transaction_id` INT(11) NULL,
+CHANGE `transaction_id` `api_transaction_id` INT(11) NULL;
