8dd7d5f2b18b7460e71ec9b7bbd78a8aa860dea9
Matt <matt@mjman.net>
2021-03-16 22:09:31 +1100

    Update sql script

--- updates/1.35.6.sql
+++ updates/1.35.6.sql
@@ -1,10 +1,14 @@
-CREATE TABLE `shop_order_lock_logs`(
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`order_id` INT(11),
-	`status_id` INT(11),
-	`locked_state` TINYINT,
-	`comment` VARCHAR(255),
+CREATE TABLE `shop_order_lock_logs` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`order_id` int(11) DEFAULT NULL,
+	`status_id` int(11) DEFAULT NULL,
+	`locked_state` tinyint(4) DEFAULT NULL,
+	`comment` varchar(255) DEFAULT NULL,
+	`created_at` datetime DEFAULT NULL,
+	`created_user_id` int(11) DEFAULT NULL,
+	`updated_at` datetime DEFAULT NULL,
+	`updated_user_id` int(11) DEFAULT NULL,
 	PRIMARY KEY (`id`),
-	INDEX (`order_id`),
-	INDEX (`status_id`)
+	KEY `order_id` (`order_id`),
+	KEY `status_id` (`status_id`)
 );
