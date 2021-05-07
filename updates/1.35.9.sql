2085e02292f94b734bba2dda737a2016d9642803
Matt <matt@mjman.net>
2021-05-07 16:05:26 +1000

    version sql

---
+++ updates/1.35.9.sql
@@ -0,0 +1,18 @@
+ALTER TABLE `shop_property_sets`
+	ADD COLUMN `api_code` VARCHAR(50);
+
+ALTER TABLE `shop_property_set_properties`
+	ADD COLUMN `required` TINYINT(4),
+	ADD COLUMN `api_code` VARCHAR(50),
+	ADD COLUMN `comment` VARCHAR(255),
+  	ADD COLUMN `validate` VARCHAR(10),
+  	ADD COLUMN `select_values` TEXT;
+
+CREATE TABLE `shop_property_sets_applied_categories` (
+	 `shop_category_id` int(11) DEFAULT NULL,
+	 `shop_property_set_id` int(11) DEFAULT NULL,
+	 KEY `set_category` (`shop_category_id`,`shop_property_set_id`)
+);
+
+ALTER TABLE `shop_product_properties`
+	ADD COLUMN `api_code` VARCHAR(50);
