4b81e21fabbe4f5b7b851d7dac961098a2dc1f84
Matt <matt@mjman.net>
2021-07-01 16:03:54 +1000

    Sql update script refactor

--- updates/1.35.11.sql
+++ updates/1.35.11.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `shop_company_information`
 	ADD COLUMN `tax_identification_number` VARCHAR(50) NULL;
+
+ALTER TABLE `shop_customer_notifications`
+	ADD  INDEX `is_system_index` (`is_system`),
+	ADD  INDEX `customer_id_index` (`customer_id`);
--- updates/1.35.12.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `shop_customer_notifications`
-	ADD  INDEX `is_system_index` (`is_system`),
-	ADD  INDEX `customer_id_index` (`customer_id`);
