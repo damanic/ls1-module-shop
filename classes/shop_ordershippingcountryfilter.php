35010a6a28e8830331414c753a252d3c06223d5b
Matt <matt@mjman.net>
2021-09-06 18:40:53 +1000

    Allow order country filters to apply to models that belong to the shop order model

--- classes/shop_ordershippingcountryfilter.php
+++ classes/shop_ordershippingcountryfilter.php
@@ -8,10 +8,19 @@
 
 		public function applyToModel($model, $keys, $context = null)
 		{
-			if ($context != 'product_report')
-				$model->where('shipping_country_id in (?)', array($keys));
-			else
-				$model->where('shop_orders.shipping_country_id in (?)', array($keys));
+			if(is_a($model,'Shop_Order')){
+				$model->where('shop_orders.shipping_country_id IN (?)', array($keys));
+			} else if ($model->belongs_to) {
+				foreach($model->belongs_to as $field => $belongs_to_info){
+					$class_name = isset($belongs_to_info['class_name']) ? $belongs_to_info['class_name'] : null;
+					if($class_name == 'Shop_Order'){
+						$foreign_key = $belongs_to_info['foreign_key'];
+						if($foreign_key){
+							$model->where($field.'_calculated_join.shipping_country_id IN (?)', array($keys));
+						}
+					}
+				}
+			}
 		}
 		
 		public function asString($keys, $context = null)
--- classes/shop_ordershippingzonefilter.php
+++ classes/shop_ordershippingzonefilter.php
@@ -11,10 +11,19 @@
 			if(!$country_ids){
 				$country_ids = array('0'); //no countries, no results
 			}
-			if ( $context != 'product_report' ) {
-				$model->where( 'shipping_country_id in (?)', array( $country_ids ) );
-			} else {
-				$model->where( 'shop_orders.shipping_country_id in (?)', array( $country_ids ) );
+
+			if(is_a($model,'Shop_Order')){
+				$model->where('shop_orders.shipping_country_id IN (?)', array($country_ids));
+			} else if ($model->belongs_to) {
+				foreach($model->belongs_to as $field => $belongs_to_info){
+					$class_name = isset($belongs_to_info['class_name']) ? $belongs_to_info['class_name'] : null;
+					if($class_name == 'Shop_Order'){
+						$foreign_key = $belongs_to_info['foreign_key'];
+						if($foreign_key){
+							$model->where($field.'_calculated_join.shipping_country_id IN (?)', array($country_ids));
+						}
+					}
+				}
 			}
 
 		}
