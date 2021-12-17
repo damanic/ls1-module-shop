f12a7a1bea65b6740f26f91128a82d491d16a1f1
Matt <matt@mjman.net>
2021-12-17 17:02:10 +1100

    Registering class loader directories for shipping types in module init.

--- init/init.php
+++ init/init.php
@@ -1,3 +1,4 @@
 <?php
+Phpr::$classLoader->add_module_directory('shipping_types');
+Phpr::$classLoader->add_module_directory('payment_types');
 Phpr::$classLoader->add_library_directory(PATH_APP.'/modules/shop/interfaces/');
-?>
